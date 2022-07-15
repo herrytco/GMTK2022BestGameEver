a50bb09483d0af898bb9d654c38c937666cdc48c
roman <scheisregal@gmail.com>
2022-07-15 22:37:37 +0200

    feat: add default impl for tile effects

--- Assets/Scripts/Interfaces/ITile.cs
+++ Assets/Scripts/Interfaces/ITile.cs
@@ -7,9 +7,43 @@ namespace Interfaces
     {
         public List<ITile> NextTiles { get; private set; } = new();
         public List<ITile> PrevTiles { get; private set; } = new();
-        
+
         public List<ICharacter> Characters { get; private set; } = new();
 
-        public List<ITileEffect> ActiveEffects { get; private set; } = new();
+        private List<ITileEffect> _activeEffects;
+
+        public void AddEffect(ITileEffect effect)
+        {
+            _activeEffects.Add(effect);
+            // TODO check if this sorts the right way around
+            _activeEffects.Sort(Comparer<ITileEffect>.Create((a, b) =>
+                -a.Priority.CompareTo(b.Priority)));
+        }
+
+        /// <summary>
+        /// Called when a character visits this tile, but it already is occupied.
+        /// </summary>
+        /// <param name="defenders">The characters already present on this tile.
+        /// Assumed to be non-empty.</param>
+        /// <param name="attacker">The visiting character.</param>
+        /// <returns>True if the attacker wins and occupies the tile.</returns>
+        public abstract bool Fight(List<ICharacter> defenders, ICharacter attacker);
+
+        public void Visit(ICharacter visitor)
+        {
+            // if the tile is occupied and the visitor loses the fight, do not process effects
+            if (Characters.Count != 0 && !Fight(Characters, visitor))
+            {
+                return;
+            }
+
+            foreach (var effect in _activeEffects)
+            {
+                if (!effect.OnCharacterVisit(this, visitor))
+                {
+                    return;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Interfaces/ITileEffect.cs
+++ Assets/Scripts/Interfaces/ITileEffect.cs
@@ -2,6 +2,13 @@
 {
     public interface ITileEffect
     {
-        
+        /// <summary>
+        /// Higher priority means this effect will get applied sooner.
+        /// The priority is expected not to change at run-time.
+        /// </summary>
+        public int Priority { get; }
+
+        /// <returns>True if the character stays and remaining effects should be applied.</returns>
+        public bool OnCharacterVisit(ITile tile, ICharacter visitor) => true;
     }
 }
