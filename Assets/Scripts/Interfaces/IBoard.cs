9180636b61fdf4b600b9e1465ec96152d95e5ea2
roman <scheisregal@gmail.com>
2022-07-15 21:22:18 +0200

    feat: define IBoard

---
+++ Assets/Scripts/Interfaces/IBoard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public abstract class IBoard
+    {
+        public List<ITile> Tiles { get; private set; }
+
+        public List<ITile> SpawnPoints { get; private set; }
+    }
+}
