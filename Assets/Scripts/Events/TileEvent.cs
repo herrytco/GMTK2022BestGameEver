44dae3ec0dfc6440810b2fdeaea989cd571dc996
Max Jakobitsch <minigugler@gmail.com>
2022-07-16 19:31:38 +0200

    feat: use EventManager in ITile

---
+++ Assets/Scripts/Events/TileEvent.cs
@@ -0,0 +1,44 @@
+﻿namespace Interfaces
+{
+    public abstract class TileEvent
+    {
+        public readonly ITile Tile;
+
+        protected TileEvent(ITile tile)
+        {
+            Tile = tile;
+        }
+    }
+
+    public class TileVisitEvent : TileEvent
+    {
+        public readonly ICharacter Character;
+
+        /// <summary>
+        /// True if the character is just passing though and will not stay on/attack the tile.
+        /// </summary>
+        public readonly bool PassThrough;
+
+        /// <summary>
+        ///     A character visits (arrives at) a tile.
+        /// </summary>
+        public TileVisitEvent(ITile tile, ICharacter character, bool passThrough) : base(tile)
+        {
+            Character = character;
+            PassThrough = passThrough;
+        }
+    }
+
+    public class TileLeaveEvent : TileEvent
+    {
+        public readonly ICharacter Character;
+
+        /// <summary>
+        ///     A character leaves (starts moving from) a tile (not just passed through).
+        /// </summary>
+        public TileLeaveEvent(ITile tile, ICharacter character) : base(tile)
+        {
+            Character = character;
+        }
+    }
+}
--- Assets/Scripts/Interfaces/ITile.cs
+++ Assets/Scripts/Interfaces/ITile.cs
@@ -8,62 +8,40 @@ using UnityEngine;
 
 namespace Interfaces
 {
+    [RequireComponent(typeof(EventManager<TileEvent>))]
     public abstract class ITile : MonoBehaviour
     {
-        protected List<ITileEffect> _activeEffects;
         public List<ITile> NextTiles { get; protected set; } = new();
         public List<ITile> PrevTiles { get; protected set; } = new();
 
-        public List<ICharacter> Characters { get; protected set; } = new();
-
-        public List<ITileEffect> ActiveEffects => _activeEffects;
-
-        public void AddEffect(ITileEffect effect)
+        public List<ICharacter> Characters
         {
-            _activeEffects.Add(effect);
-            // TODO check if this sorts the right way around
-            _activeEffects.Sort(Comparer<ITileEffect>.Create((a, b) =>
-                -a.Priority.CompareTo(b.Priority)));
+            get => _characters;
+            protected set => _characters = value;
         }
 
-        public void RemoveEffectsWhere(Predicate<ITileEffect> match)
+        private List<ICharacter> _characters = new();
+
+        protected EventManager<TileEvent> EventManager
         {
-            _activeEffects.RemoveAll(match);
+            get
+            {
+                if (!ReferenceEquals(_eventManager, null)) _eventManager = GetComponent<EventManager<TileEvent>>();
+                return _eventManager;
+            }
         }
+
+        private EventManager<TileEvent> _eventManager;
+
+        public void AddObserver([NotNull] IEventObserver<TileEvent> obs) => EventManager.RegisterObserver(obs);
+        public void RemoveObserver([NotNull] IEventObserver<TileEvent> obs) => EventManager.DeregisterObserver(obs);
 
         /// <summary>
         ///     A character passes through this tile (does not stay/try to occupy)
         /// </summary>
         public void Visit(ICharacter visitor, [NotNull] Action<ITile, ICharacter> onDone)
         {
-            HandleEffectAction = effect =>
-            {
-                return effect.OnVisit(this, visitor, () => StartCoroutine(HandleEffects()));
-            };
-
-            OnEffectsHandled = () => onDone(this, visitor);
-
-            StopCoroutine(HandleEffects());
-            StartCoroutine(HandleEffects());
-        }
-
-        /// <summary>
-        ///     Called when a character tries to occupy this tile, but it already is occupied.
-        /// </summary>
-        /// <param name="defenders">
-        ///     The characters already present on this tile.
-        ///     Assumed to be non-empty.
-        /// </param>
-        /// <param name="attacker">The visiting character.</param>
-        /// <returns>True if the attacker wins and occupies the tile.</returns>
-        public bool Fight(List<ICharacter> defenders, ICharacter attacker)
-        {
-            ITileEffect deathEffect = defenders[0].gameObject.AddComponent<CharacterDeathEffect>();
-            AddEffect(deathEffect);
-
-            Characters = new List<ICharacter> { attacker };
-
-            return true;
+            EventManager.Emit(new TileVisitEvent(this, visitor, true), () => onDone(this, visitor));
         }
 
         /// <summary>
@@ -71,22 +49,7 @@ namespace Interfaces
         /// </summary>
         public void Occupy(ICharacter attacker, [NotNull] Action<ITile, ICharacter> onDone)
         {
-            // if the tile is occupied and the visitor loses the fight, do not process effects
-            if (Characters.Count != 0 && !Fight(Characters, attacker))
-            {
-                onDone(this, attacker);
-                return;
-            }
-
-            HandleEffectAction = effect =>
-            {
-                return effect.OnOccupied(this, attacker, () => StartCoroutine(HandleEffects()));
-            };
-
-            OnEffectsHandled = () => onDone(this, attacker);
-
-            StopCoroutine(HandleEffects());
-            StartCoroutine(HandleEffects());
+            EventManager.Emit(new TileVisitEvent(this, attacker, false), () => onDone(this, attacker));
         }
 
         /// <summary>
@@ -94,57 +57,11 @@ namespace Interfaces
         /// </summary>
         public void Leave(ICharacter character, ITile destination, [NotNull] Action<ITile, ICharacter> onDone)
         {
-            HandleEffectAction = effect =>
+            EventManager.Emit(new TileLeaveEvent(this, character), () =>
             {
-                return effect.OnLeave(this, character, destination, () => StartCoroutine(HandleEffects()));
-            };
-
-            OnEffectsHandled = () => onDone(this, character);
-
-            StopCoroutine(HandleEffects());
-            StartCoroutine(HandleEffects());
-
-            Characters.RemoveAll(c => c == character);
-        }
-
-        public Action OnEffectsHandled
-        {
-            get => _onEffectsHandled;
-            protected set
-            {
-                if (value != null && _onEffectsHandled != null)
-                    Debug.LogWarning(
-                        "replacing existing OnEffectsHandled; coroutine may not have terminated correctly");
-                if (value == null)
-                    StopCoroutine(HandleEffects());
-                _onEffectsHandled = value;
-            }
-        }
-
-        private Action _onEffectsHandled;
-
-        protected Func<ITileEffect, TileEffectResult> HandleEffectAction;
-
-        protected IEnumerator HandleEffects()
-        {
-            foreach (var effect in _activeEffects.ToList())
-            {
-                TileEffectResult result = HandleEffectAction(effect);
-                if (!result.Done) yield return null; // pause execution until next call
-                
-                Destroy(effect.gameObject);
-                
-                if (result.CharacterRemoved) break;
-            }
-
-            var onEffectsHandled = _onEffectsHandled;
-            _onEffectsHandled = null;
-            onEffectsHandled();
-        }
-
-        private void OnDestroy()
-        {
-            StopAllCoroutines();
+                _characters = new();
+                onDone(this, character);
+            });
         }
     }
 }
