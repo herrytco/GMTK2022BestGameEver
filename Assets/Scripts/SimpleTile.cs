f42d581c2046ba520b9d182769f334dab5b5abc7
Max Jakobitsch <minigugler@gmail.com>
2022-07-16 08:54:04 +0200

    feat: simple tile protoype impl

---
+++ Assets/Scripts/SimpleTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Interfaces;
+
+public class SimpleTile : ITile
+{
+    public SimpleTile[] myNextTiles;
+
+    private void Start()
+    {
+        NextTiles = new List<ITile>(myNextTiles);
+        
+        PrevTiles = new();
+        foreach (var tile in NextTiles)
+        {
+            tile.PrevTiles.Add(this);
+        }
+    }
+
+    public override bool Fight(List<ICharacter> defenders, ICharacter attacker)
+    {
+        throw new System.NotImplementedException();
+    }
+}
