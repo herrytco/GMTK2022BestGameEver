f1a8d46533a579231009de138be66d2752342d71
Max Jakobitsch <minigugler@gmail.com>
2022-07-16 18:52:23 +0200

    feat: introduce EventManager

---
+++ Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Interfaces
+{
+    public class EventManager<TEvent> : MonoBehaviour
+    {
+        private List<IEventObserver<TEvent>> _observers = new();
+
+        public List<IEventObserver<TEvent>> Observers
+        {
+            get => new(_observers);
+            protected set => _observers = value;
+        }
+
+        public virtual void RegisterObserver(IEventObserver<TEvent> observer)
+        {
+            var tmpObservers = Observers;
+            tmpObservers.Add(observer);
+            // TODO check if this sorts the right way around
+            tmpObservers.Sort(
+                Comparer<IEventObserver<TEvent>>.Create(
+                    (a, b) => -a.Priority.CompareTo(b.Priority)
+                )
+            );
+            _observers = tmpObservers;
+        }
+
+        public virtual void DeregisterObserver(IEventObserver<TEvent> observer)
+        {
+            _observers.Remove(observer);
+            _toDoObservers.Remove(observer);
+        }
+
+        /// <summary>
+        ///     List of observers that currently wait to be notified. Only used while an event is processed. 
+        /// </summary>
+        private List<IEventObserver<TEvent>> _toDoObservers = new();
+
+        private TEvent _toDoEvent;
+        private Action _onDone;
+
+        public void Emit(TEvent evnt, Action onDone)
+        {
+            _toDoEvent = evnt;
+            _onDone = onDone;
+            StartCoroutine(HandleEvents());
+        }
+
+        protected IEnumerator HandleEvents()
+        {
+            _toDoObservers = Observers;
+            while (_toDoObservers.Count != 0)
+            {
+                var obs = _toDoObservers[0];
+                _toDoObservers.RemoveAt(0);
+                
+                obs.OnEvent(_toDoEvent, ResumeHandleEvents);
+                if (!obs.isDone) yield return null;
+                if (!ReferenceEquals(obs.destroyWhenDone, null))
+                {
+                    Destroy(obs.destroyWhenDone);
+                }
+            }
+
+            _onDone();
+        }
+
+        private Action ResumeHandleEvents
+        {
+            get { return () => StartCoroutine(HandleEvents()); }
+        }
+    }
+}
---
+++ Assets/Scripts/Events/EventObserver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IEventObserver<in TEvent>
+    {
+        public int Priority { get; }
+        
+        public bool isDone { get; }
+        public GameObject destroyWhenDone { get; }
+        
+        /// <summary>
+        /// Notify of an event.
+        ///
+        /// Assumed to EITHER: have "isDone = true" when this method returns,
+        /// OR: call onDone when the event has been properly processed.
+        /// </summary>
+        public void OnEvent(TEvent evnt, Action onDone);
+    }
+}
