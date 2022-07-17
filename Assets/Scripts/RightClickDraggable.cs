d8499f719822763345744cc18f1aac1dad1209ad
Manuel Herold <herrytco@gmail.com>
2022-07-17 15:32:19 +0200

    feat: added draggable script

---
+++ Assets/Scripts/RightClickDraggable.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightClickDraggable : MonoBehaviour
+{
+    private Camera _camera;
+    private bool _currentlyDragging = false;
+
+    private Vector3 _mouseOrig;
+    private Vector3 _objectOrig;
+
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && !_currentlyDragging)
+        {
+            _currentlyDragging = true;
+            _mouseOrig = _camera.ScreenToWorldPoint(Input.mousePosition);
+            _objectOrig = transform.position;
+        }
+
+        if (Input.GetMouseButton(1) && _currentlyDragging)
+        {
+            Vector3 mouseCurr = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 mov = mouseCurr - _mouseOrig;
+            mov.z = 0;
+
+            transform.position = _objectOrig + mov;
+        }
+
+        if (Input.GetMouseButtonUp(1) && _currentlyDragging)
+        {
+            _currentlyDragging = false;
+        }
+    }
+}
